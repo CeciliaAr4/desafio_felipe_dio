b1834e653f2411e3da80c8fed17a15935ebeb444
Cecília Araújo <Ceciliaar4ujo@gmail.com>
2024-06-10 12:57:24 -0300

    Update desafioHero.js

--- desafioHero.js
+++ desafioHero.js
@@ -1,7 +1,24 @@
 
-let NomeHero
-let xpHero
+nome = input("Digite o nome do herói: ")
+xpHero = int(input("Digite seu nivel de Xp"))
 
 if (xpHero >= 1000){
-    xpHero = Ferro
+    xpHero = "Ferro";
+} else if (xpHero >= 1001 && xpHero <=2000){
+    nivel = "Bronze";
+} else if (xpHero >= 2001 && xpHero <=5000){
+    nivel = "Prata";
+} else if (xpHero >= 5001 && xpHero <=7000){
+    nivel = "Ouro";
+} else if (xpHero >= 7001 && xpHero <=8000){
+    nivel = "Platina";
+} else if (xpHero >= 8001 && xpHero <=9000){
+    nivel = "Ascendente";
+} else if (xpHero >= 9001 && xpHero <=10000){
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
 }
+
+
+alert('O Herói de nome ${ nome } está no nível de ${ nivel }'); 
